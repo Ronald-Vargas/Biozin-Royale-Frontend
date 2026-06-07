1131d56a2e91dcf8a99eb8da916e92857c63bd4c
Ronald-Vargas <ronald.vargasgo@gmail.com>
2026-06-06 20:30:53 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Biozin-Royale-Frontend',
+  webDir: 'www'
+};
+
+export default config;
